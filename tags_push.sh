1f3f6b7dcd8bd40498cc7e1ad0d394818562dba7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2023-01-09 20:16:57 +0800

    :bug: 修正批量推送的命令

--- tags_push.sh
+++ tags_push.sh
@@ -3,6 +3,5 @@ images=$(docker images | grep $DOCKER_USERNAME/gitlab-runner-helper | awk '{prin
 for image in $images
 do
   echo "准备推送：$image"
-  echo docker push $image
-  echo "推送完成：$image"
+  docker push $image && echo "推送完成：$image"
 done
